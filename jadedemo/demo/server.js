6c0f93ac12a427f1fb9ba3555ab6f0d105fcbe7a
jfengsky <jfengsky@gmail.com>
2013-07-10 17:57:55 +0800

    modify server.js

--- jadedemo/demo/server.js
+++ jadedemo/demo/server.js
@@ -1,8 +1,13 @@
-var http = require('http');
+var server,
+  ip = "127.0.0.1",
+  port = 8888,
+  http = require('http');
 
-http.createServer(function(request, response){
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("hello world");
-  response.end();
-}).listen(8888);
-console.log('server is create');
+server = http.createServer(function(req, res){
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Head World');
+}).listen(port, ip);
+
+console.log("server is running" + ip + ":" + port);
