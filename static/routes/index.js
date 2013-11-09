8670a69e279ee2153d5d7811c5d0c623ebc79934
jfeng <jfengsky@gmail.com>
2013-11-09 16:53:00 +0800

    static modify

--- static/routes/index.js
+++ static/routes/index.js
@@ -2,24 +2,9 @@
 /*
  * GET home page.
  */
-var fs = require('fs');
-var path = require('path');
-
-fs.readFile('./views/index.jade','ascii' , function(err, data){
-
-  console.log(data);
-  fs.writeFile('./html/demo.html', data, 'ascii', function(err){
-    console.log('save success');
-  });
-
-});
-
-
 
 module.exports = function(app){
   app.get('/', function(req, res){
     res.render('index', { title: 'index'});
-
   });
-
 };
