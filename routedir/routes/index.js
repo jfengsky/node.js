c198f528cea6d9a976f887393a87255c16b6fcea
jfeng <jfengsky@gmail.com>
2013-11-25 21:08:41 +0800

    route modify

--- routedir/routes/index.js
+++ routedir/routes/index.js
@@ -12,11 +12,12 @@ module.exports = function(app){
     fileFilter: [ '*.jade' ]
   }).on('data', function (entry) {
 
-    console.log(entry.path)
+    // console.log(entry.path);
     var url = entry.path.slice(0, -5);
+    console.log(url);
     
     app.get('/' + url + '.html', function(req, res){
-      res.render('index', { title: 'index'});
+      res.render(url, { title: 'index'});
     });
 
   });
