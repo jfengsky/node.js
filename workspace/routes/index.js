ba0d4ba0a049a8574183fb3846be7932ceb1909c
jfengsky <jfengsky@gmail.com>
2015-03-09 00:12:30 +0800

    visa modify

--- workspace/routes/index.js
+++ workspace/routes/index.js
@@ -266,88 +266,92 @@ var addressphone3 = {
 var PptVisa4 = {
     "Country": "美国",
     "Pages": [{
-        "PageName": "第四页，护照信息页面",
+        "PageName": "第四页 护照信息",
         "PageUrl": "https://ceac.state.gov/GenNIV/General/complete/Passport_Visa_Info.aspx?node=PptVisa",
         "Values": [{
-            "ColumnName": "护照类型",
+            "ColumnName": "护照/旅行证件种类",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPPT_TYPE",
-            "Value": "O"
+            "Value": "T"
         }, {
-            "ColumnName": "护照号码",
+            "ColumnName": "护照种类其他的说明[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPptOtherExpl",
+            "Value": "salkfjowaijroqwjjlasdkjflajsd"
+        }, {
+            "ColumnName": "护照/旅行证件号码",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPPT_NUM",
-            "Value": "323232323"
+            "Value": "88888888888888"
         }, {
-            "ColumnName": "护照本号码",
+            "ColumnName": "护照本编号",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPPT_BOOK_NUM",
-            "Value": "45454545"
+            "Value": "123344556"
         }, {
-            "ColumnName": "护照本号码不适用的",
+            "ColumnName": "护照本编号（不适用）",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_cbxPPT_BOOK_NUM_NA",
-            "Value": "True"
+            "Value": "False"
         }, {
-            "ColumnName": "发放护照的国家",
+            "ColumnName": "颁发护照/旅行证件的国家/机构[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPPT_ISSUED_CNTRY",
-            "Value": "ASMO"
+            "Value": "CHIN"
         }, {
-            "ColumnName": "发放护照的城市",
+            "ColumnName": "护照签发地—城市[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPPT_ISSUED_IN_CITY",
-            "Value": "ShangHai"
+            "Value": "zhengzhou"
         }, {
-            "ColumnName": "发放护照的省/州",
+            "ColumnName": "护照签发地—州/省份（如果护照上显示）[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPPT_ISSUED_IN_STATE",
-            "Value": "ZheJiang"
+            "Value": "henan"
         }, {
-            "ColumnName": "发放护照的地带",
-            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_lblPPT_ISSUED_IN_CNTRY",
-            "Value": "ASMO"
+            "ColumnName": "护照签发地—国家/地区[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPPT_ISSUED_IN_CNTRY",
+            "Value": "CHIN"
         }, {
-            "ColumnName": "发放日期[日]",
+            "ColumnName": "签发日期-日",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPPT_ISSUED_DTEDay",
-            "Value": "3"
+            "Value": "1"
         }, {
-            "ColumnName": "发放日期[月]",
+            "ColumnName": "签发日期-月",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPPT_ISSUED_DTEMonth",
-            "Value": "6"
+            "Value": "2"
         }, {
-            "ColumnName": "发放日期[年]",
+            "ColumnName": "签发日期-年",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPPT_ISSUEDYear",
-            "Value": "2004"
+            "Value": "2010"
         }, {
-            "ColumnName": "过期日期[日]",
+            "ColumnName": "失效日期-日",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPPT_EXPIRE_DTEDay",
-            "Value": "4"
+            "Value": "1"
         }, {
-            "ColumnName": "过期日期[月]",
+            "ColumnName": "失效日期-月",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPPT_EXPIRE_DTEMonth",
-            "Value": "9"
+            "Value": "1"
         }, {
-            "ColumnName": "过期日期[年]",
+            "ColumnName": "失效日期-年",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPPT_EXPIREYear",
-            "Value": "2040"
+            "Value": "2019"
         }, {
-            "ColumnName": "过期不适用的",
+            "ColumnName": "失效日期（不适用）",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_cbxPPT_EXPIRE_NA",
             "Value": "False"
         }, {
-            "ColumnName": "是否遗失过护照",
+            "ColumnName": "您的护照是否曾遗失或者被盗？",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_rblLOST_PPT_IND_0",
             "Value": "True"
         }, {
-            "ColumnName": "遗失护照号",
+            "ColumnName": "丢失的护照/旅行证件号码",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dtlLostPPT_ctl00_tbxLOST_PPT_NUM",
-            "Value": "333333"
+            "Value": "9998887776655443"
         }, {
-            "ColumnName": "遗失护照号不适用的",
+            "ColumnName": "丢失的护照/旅行证件号码（忘记）",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dtlLostPPT_ctl00_cbxLOST_PPT_NUM_UNKN_IND",
             "Value": "False"
         }, {
-            "ColumnName": "遗失护照发放国家",
+            "ColumnName": "丢失的颁发护照/旅行证件的国家/机构[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dtlLostPPT_ctl00_ddlLOST_PPT_NATL",
-            "Value": "ASMO"
+            "Value": "CHIN"
         }, {
-            "ColumnName": "遗失护照说明",
+            "ColumnName": "丢失的说明[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dtlLostPPT_ctl00_tbxLOST_PPT_EXPL",
-            "Value": "explain explain explain"
+            "Value": "asdfasfaewerwerwe"
         }]
     }]
 };
@@ -355,91 +359,249 @@ var PptVisa4 = {
 var Travel5 = {
     "Country": "美国",
     "Pages": [{
-        "PageName": "第五页，旅行信息",
+        "PageName": "第五页 旅行信息",
         "PageUrl": "https://ceac.state.gov/GenNIV/General/complete/complete_travel.aspx?node=Travel",
         "Values": [{
-            "ColumnName": "去美国的目的",
+            "ColumnName": "赴美访问目的",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dlPrincipalAppTravel_ctl00_ddlPurposeOfTrip",
-            "Value": "TD/TN"
+            "Value": "B"
         }, {
-            "ColumnName": "具体说明",
-            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPPT_NUM",
-            "Value": "323232323"
+            "ColumnName": "赴美访问目的具体说明",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dlPrincipalAppTravel_ctl00_ddlOtherPurpose",
+            "Value": "B1-B2"
         }, {
-            "ColumnName": "是否制定了具体的旅行计划",
+            "ColumnName": "您是否已经制定了具体的旅行计划？",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_rblSpecificTravel_0",
             "Value": "True"
         }, {
-            "ColumnName": "到达美国日期[日]",
+            "ColumnName": "到达美国日期-日",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlARRIVAL_US_DTEDay",
-            "Value": "3"
+            "Value": "1"
         }, {
-            "ColumnName": "到达美国日期[月]",
+            "ColumnName": "到达美国日期-月",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlARRIVAL_US_DTEMonth",
-            "Value": "5"
+            "Value": "4"
         }, {
-            "ColumnName": "到达美国日期[年]",
+            "ColumnName": "到达美国日期-年",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxARRIVAL_US_DTEYear",
             "Value": "2015"
         }, {
             "ColumnName": "到达航班",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxArriveFlight",
-            "Value": "TFT101"
+            "Value": ""
         }, {
-            "ColumnName": "抵达城市",
+            "ColumnName": "抵达城市[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxArriveCity",
-            "Value": "Washton"
+            "Value": "niuyue"
         }, {
-            "ColumnName": "离开美国日期[日]",
+            "ColumnName": "离开美国日期-日",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlDEPARTURE_US_DTEDay",
-            "Value": "23"
+            "Value": "5"
         }, {
-            "ColumnName": "离开美国日期[月]",
+            "ColumnName": "离开美国日期-月",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlDEPARTURE_US_DTEMonth",
-            "Value": "3"
+            "Value": "5"
         }, {
-            "ColumnName": "离开美国日期[年]",
+            "ColumnName": "离开美国日期-年",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxDEPARTURE_US_DTEYear",
-            "Value": "2016"
+            "Value": "2015"
         }, {
             "ColumnName": "离开航班",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxDepartFlight",
-            "Value": "WD201"
+            "Value": ""
         }, {
-            "ColumnName": "离美城市",
+            "ColumnName": "离美城市[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxDepartCity",
-            "Value": "Lost"
+            "Value": "niuyue"
         }, {
-            "ColumnName": "计划在美国访问的城市",
+            "ColumnName": "请提供您在美期间计划访问的地点名称[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dtlTravelLoc_ctl00_tbxSPECTRAVEL_LOCATION",
-            "Value": "BDSDS"
+            "Value": "asdfasd"
         }, {
-            "ColumnName": "住在美国的街道1",
+            "ColumnName": "计划到达日期-日",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlTRAVEL_DTEDay",
+            "Value": "1"
+        }, {
+            "ColumnName": "计划到达日期-月",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlTRAVEL_DTEMonth",
+            "Value": "4"
+        }, {
+            "ColumnName": "计划到达日期-年",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxTRAVEL_DTEYear",
+            "Value": "2015"
+        }, {
+            "ColumnName": "计划在美停留时间-数字",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxTRAVEL_LOS",
+            "Value": "50"
+        }, {
+            "ColumnName": "计划在美停留时间-单位",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlTRAVEL_LOS_CD",
+            "Value": "D"
+        }, {
+            "ColumnName": "在美停留期间的住址-街道地址（第一行）[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxStreetAddress1",
-            "Value": "FuChuan road 99"
+            "Value": "asdfoiweorjwljlsadjfl"
         }, {
-            "ColumnName": "住在美国的街道2",
+            "ColumnName": "在美停留期间的住址-街道地址（第二行）[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxStreetAddress2",
-            "Value": "ChangNing"
+            "Value": ""
         }, {
-            "ColumnName": "住在美国的城市",
+            "ColumnName": "在美停留期间的住址-城市[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxCity",
-            "Value": "aasbcd"
+            "Value": "niuyue"
         }, {
-            "ColumnName": "住在美国的州",
+            "ColumnName": "在美停留期间的住址-州[英文]",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlTravelState",
-            "Value": "AR"
+            "Value": "CA"
         }, {
-            "ColumnName": "州的编码",
+            "ColumnName": "在美停留期间的住址-邮政编码",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbZIPCode",
-            "Value": "200231"
+            "Value": "12345"
         }, {
-            "ColumnName": "付费",
+            "ColumnName": "支付您旅行费用的个人或组织名称",
             "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlWhoIsPaying",
-            "Value": "H"
+            "Value": "O"
+        }, {
+            "ColumnName": "承担您旅行费用者的姓氏[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerSurname",
+            "Value": "zhang"
+        }, {
+            "ColumnName": "承担您旅行费用者的名字[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerGivenName",
+            "Value": "san"
+        }, {
+            "ColumnName": "承担您旅行费用者的电话号码",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerPhone",
+            "Value": "08618101654965"
+        }, {
+            "ColumnName": "承担您旅行费用者的电子邮件地址",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPAYER_EMAIL_ADDR",
+            "Value": "zhang.chui@qq.com"
+        }, {
+            "ColumnName": "承担您旅行费用者与您关系[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_cbxDNAPAYER_EMAIL_ADDR_NA",
+            "Value": "F"
+        }, {
+            "ColumnName": "承担您旅行费用者与您关系[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPayerRelationship",
+            "Value": "F"
+        }, {
+            "ColumnName": "承担您旅行费用者地址是否与您家庭或右击地址相同",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_rblPayerAddrSameAsInd_1",
+            "Value": "False"
+        }, {
+            "ColumnName": "承担您旅行费用者街道地址（第一行）[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerStreetAddress1",
+            "Value": "shanghaishichangningqufuquanlu"
+        }, {
+            "ColumnName": "承担您旅行费用者街道地址（第二行）[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerStreetAddress2",
+            "Value": ""
+        }, {
+            "ColumnName": "承担您旅行费用城市[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerCity",
+            "Value": "shanghai"
+        }, {
+            "ColumnName": "承担您旅行费用州/省份[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerStateProvince",
+            "Value": "SHangHai"
+        }, {
+            "ColumnName": "承担您旅行费用州/省份不适用的",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_cbxDNAPayerStateProvince",
+            "Value": "False"
+        }, {
+            "ColumnName": "承担您旅行费用邮政编码",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerPostalZIPCode",
+            "Value": "100000"
+        }, {
+            "ColumnName": "承担您旅行费用邮政编码不适用的",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_cbxDNAPayerPostalZIPCode",
+            "Value": "False"
+        }, {
+            "ColumnName": "承担您旅行费用国家[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPayerCountry",
+            "Value": "CHIN"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayingCompany",
+            "Value": "xiecheng"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称电话号码",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerPhone",
+            "Value": "08602123456789"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称与您的关系[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxCompanyRelation",
+            "Value": "shangxiaji"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称街道地址（第一行）[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerStreetAddress1",
+            "Value": "shanghaishichangningqufuquanlu"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称街道地址（第二行）[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerStreetAddress2",
+            "Value": ""
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称城市[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerCity",
+            "Value": "shanghai"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称州/省份[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerStateProvince",
+            "Value": "ShangHai"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称州/省份不适用的",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_cbxDNAPayerStateProvince",
+            "Value": "False"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称邮政编码",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxPayerPostalZIPCode",
+            "Value": "200000"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称邮政编码不适用的",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_cbxDNAPayerPostalZIPCode",
+            "Value": "False"
+        }, {
+            "ColumnName": "承担您旅行费用的公司或组织名称国家[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_ddlPayerCountry",
+            "Value": "CHIN"
         }]
     }]
 };
+
+var TravelCompanions6 = {
+    "Country": "美国",
+    "Pages": [{
+        "PageName": "第六页 旅行同伴",
+        "PageUrl": "https://ceac.state.gov/GenNIV/General/complete/complete_travelcompanions.aspx?node=TravelCompanions",
+        "Values": [{
+            "ColumnName": "是否有人与您同行？",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_rblOtherPersonsTravelingWithYou_0",
+            "Value": "True"
+        }, {
+            "ColumnName": "您是否作为一个团队或者组织的成员去旅行？",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_rblGroupTravel_1",
+            "Value": "False"
+        }, {
+            "ColumnName": "输入您旅行团队的名称[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_tbxGroupName",
+            "Value": "ZhongGuoChunQiuLvXingShe"
+        }, {
+            "ColumnName": "随行人员的姓氏[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dlTravelCompanions_ctl00_tbxSurname",
+            "Value": "li"
+        }, {
+            "ColumnName": "随行人员的名字[英文]",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dlTravelCompanions_ctl00_tbxGivenName",
+            "Value": "si"
+        }, {
+            "ColumnName": "随行人员和您的关系",
+            "FormId": "ctl00_SiteContentPlaceHolder_FormView1_dlTravelCompanions_ctl00_ddlTCRelationship",
+            "Value": "F"
+        }]
+    }]
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
@@ -475,6 +637,12 @@ router.get('/VisaAutoCompleteApi', function(req, res, next) {
             break;
         case 'Travel':
             sendData = Travel5;
+            break;
+        case 'TravelCompanions':
+            sendData = TravelCompanions6;
+            break;
+        case 'PreviousUSTravel':
+            sendData = PreviousUSTravel7;
             break;
     };
     res.send(sendData);
